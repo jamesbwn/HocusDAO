3d7c54f4d33b55d4b98c16645b67c73a4a1540ec
raxioma <raxioma@outlook.com>
2021-12-20 06:22:08 +1000

    update contract

--- src/helpers/AllBonds.ts
+++ src/helpers/AllBonds.ts
@@ -25,16 +25,16 @@ import { abi as ierc20Abi } from "src/abi/IERC20.json";
 
 // TODO(zx): Further modularize by splitting up reserveAssets into vendor token definitions
 //   and include that in the definition of a bond
-export const mim = new StableBond({
-  name: "mim",
-  displayName: "MIM",
-  bondToken: "MIM",
-  bondIconSvg: MimImg,
+export const dai = new StableBond({
+  name: "dai",
+  displayName: "DAI",
+  bondToken: "DAI",
+  bondIconSvg: DaiImg,
   bondContractABI: DaiBondContract,
   networkAddrs: {
     [NetworkID.Mainnet]: {
-      bondAddress: "0x654d5796B2C70D24eD7402e87D8a72BddF87ccca",
-      reserveAddress: "0x130966628846bfd36ff31a822705796e8cb8c18d",
+      bondAddress: "0x789cCA841CdC2a77dDEaBC0A9Fdb63FAE6E1C99D",
+      reserveAddress: "0xbA7dEebBFC5fA1100Fb055a87773e1E99Cd3507a",
     },
     [NetworkID.Testnet]: {
       bondAddress: "0xDea5668E815dAF058e3ecB30F645b04ad26374Cf",
@@ -42,59 +42,76 @@ export const mim = new StableBond({
     },
   },
 });
-export const usdt = new StableBond({
-  name: "usdt",
-  displayName: "USDT",
-  bondToken: "USDT",
-  decimals: 6,
-  bondIconSvg: UsdtImg,
-  bondContractABI: DaiBondContract,
-  networkAddrs: {
-    [NetworkID.Mainnet]: {
-      bondAddress: "0x3e185190f044B3c887b65c11657B0d5433560618",
-      reserveAddress: "0xc7198437980c041c805a1edcba50c1ce5db95118"
-    },
-    [NetworkID.Testnet]: {
-      bondAddress: "",
-      reserveAddress: "",
-    },
-  },
-});
-export const avax = new CustomBond({
-  name: "avax",
-  displayName: "wAVAX",
-  lpUrl: "",
-  bondType: BondType.StableAsset,
-  bondToken: "WAVAX",
-  bondIconSvg: wAVAXImg,
-  bondContractABI: EthBondContract,
-  reserveContract: ierc20Abi, // The Standard ierc20Abi since they're normal tokens
-  networkAddrs: {
-    [NetworkID.Mainnet]: {
-      bondAddress: "0x63933d4e91C84baE6577744fd75c4f4f2C44d901",
-      reserveAddress: "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7",
-    },
-    [NetworkID.Testnet]: {
-      bondAddress: "0xca7b90f8158A4FAA606952c023596EE6d322bcf0",
-      reserveAddress: "0xc778417e063141139fce010982780140aa0cd5ab",
-    },
-  },
-  customTreasuryBalanceFunc: async function (this: CustomBond, networkID, provider) {
-    const ethBondContract = this.getContractForBond(networkID, provider);
-    let ethPrice = await ethBondContract.assetPrice();
-    ethPrice = ethPrice / Math.pow(10, 8);
-    const token = this.getContractForReserve(networkID, provider);
-    let avaxAmount = await token.balanceOf(addresses[networkID].TREASURY_ADDRESS);
-    avaxAmount = avaxAmount / Math.pow(10, 18);
-    return avaxAmount * ethPrice;
-  },
-});
+// export const mim = new StableBond({
+//   name: "mim",
+//   displayName: "MIM",
+//   bondToken: "MIM",
+//   bondIconSvg: MimImg,
+//   bondContractABI: DaiBondContract,
+//   networkAddrs: {
+//     [NetworkID.Mainnet]: {
+//       bondAddress: "0x654d5796B2C70D24eD7402e87D8a72BddF87ccca",
+//       reserveAddress: "0x130966628846bfd36ff31a822705796e8cb8c18d",
+//     },
+//     [NetworkID.Testnet]: {
+//       bondAddress: "0xDea5668E815dAF058e3ecB30F645b04ad26374Cf",
+//       reserveAddress: "0xB2180448f8945C8Cc8AE9809E67D6bd27d8B2f2C",
+//     },
+//   },
+// });
+// export const usdt = new StableBond({
+//   name: "usdt",
+//   displayName: "USDT",
+//   bondToken: "USDT",
+//   decimals: 6,
+//   bondIconSvg: UsdtImg,
+//   bondContractABI: DaiBondContract,
+//   networkAddrs: {
+//     [NetworkID.Mainnet]: {
+//       bondAddress: "0x3e185190f044B3c887b65c11657B0d5433560618",
+//       reserveAddress: "0xc7198437980c041c805a1edcba50c1ce5db95118"
+//     },
+//     [NetworkID.Testnet]: {
+//       bondAddress: "",
+//       reserveAddress: "",
+//     },
+//   },
+// });
+// export const avax = new CustomBond({
+//   name: "avax",
+//   displayName: "wAVAX",
+//   lpUrl: "",
+//   bondType: BondType.StableAsset,
+//   bondToken: "WAVAX",
+//   bondIconSvg: wAVAXImg,
+//   bondContractABI: EthBondContract,
+//   reserveContract: ierc20Abi, // The Standard ierc20Abi since they're normal tokens
+//   networkAddrs: {
+//     [NetworkID.Mainnet]: {
+//       bondAddress: "0x63933d4e91C84baE6577744fd75c4f4f2C44d901",
+//       reserveAddress: "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7",
+//     },
+//     [NetworkID.Testnet]: {
+//       bondAddress: "0xca7b90f8158A4FAA606952c023596EE6d322bcf0",
+//       reserveAddress: "0xc778417e063141139fce010982780140aa0cd5ab",
+//     },
+//   },
+//   customTreasuryBalanceFunc: async function (this: CustomBond, networkID, provider) {
+//     const ethBondContract = this.getContractForBond(networkID, provider);
+//     let ethPrice = await ethBondContract.assetPrice();
+//     ethPrice = ethPrice / Math.pow(10, 8);
+//     const token = this.getContractForReserve(networkID, provider);
+//     let avaxAmount = await token.balanceOf(addresses[networkID].TREASURY_ADDRESS);
+//     avaxAmount = avaxAmount / Math.pow(10, 18);
+//     return avaxAmount * ethPrice;
+//   },
+// });
 
-export const papa_mim = new LPBond({
-  name: "papa_mim_lp",
-  displayName: "PAPA-MIM LP",
-  bondToken: "MIM",
-  bondIconSvg: PAPAMimimg,
+export const hocus_dai = new LPBond({
+  name: "hocus_dai_lp",
+  displayName: "HOCUS-DAI LP",
+  bondToken: "HOCUS DAI LP",
+  bondIconSvg: HecDaiimg,
   bondContractABI: BondHecDaiContract,
   reserveContract: ReserveHecDaiContract,
   networkAddrs: {
@@ -108,14 +125,34 @@ export const papa_mim = new LPBond({
     },
   },
   lpUrl:
-    "https://traderjoexyz.com/#/pool/0x70b33ebC5544C12691d055b49762D0F8365d99Fe/0x130966628846BFd36ff31a822705796e8cb8C18D",
+    "https://traderjoexyz.com/#/pool/0x74572964d285E2f82940ccE11f1D55a1d168818f/0xbA7dEebBFC5fA1100Fb055a87773e1E99Cd3507a",
 });
 
+export const hocus_mim = new LPBond({
+  name: "hocus_mim_lp",
+  displayName: "HOCUS-MIM LP",
+  bondToken: "HOCUS MIM LP",
+  bondIconSvg: PAPAMimimg,
+  bondContractABI: BondHecDaiContract,
+  reserveContract: ReserveHecDaiContract,
+  networkAddrs: {
+    [NetworkID.Mainnet]: {
+      bondAddress: "0x50dbed4742Bf180eeAFd9a936f40FFc7Fd9DA9df",
+      reserveAddress: "0xc6fe703ef2dfc165bec01080dadc8d70f75fe395",
+    },
+    [NetworkID.Testnet]: {
+      bondAddress: "0xcF449dA417cC36009a1C6FbA78918c31594B9377",
+      reserveAddress: "0x8D5a22Fb6A1840da602E56D1a260E56770e0bCE2",
+    },
+  },
+  lpUrl:
+    "https://traderjoexyz.com/#/pool/0x74572964d285E2f82940ccE11f1D55a1d168818f/0x130966628846BFd36ff31a822705796e8cb8C18D",
+});
 // HOW TO ADD A NEW BOND:
 // Is it a stableCoin bond? use `new StableBond`
 // Is it an LP Bond? use `new LPBond`
 // Add new bonds to this array!!
-export const allBonds = [papa_mim, mim, usdt, avax];
+export const allBonds = [dai, hocus_dai, hocus_mim];
 export const allBondsMap = allBonds.reduce((prevVal, bond) => {
   return { ...prevVal, [bond.name]: bond };
 }, {});
